1182fcb74baa17e21c11e25164d43b4cdeeac7c4
Daniel <deumolo@gmail.com>
2023-10-07 11:47:27 -0600

    Added meetups grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,21 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import meetups from "./meetups.js";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
 </script>
 
 <Header />
+
+<section id="meetups">
+    <MeetupGrid {meetups} />    
+</section>
+
+<style>
+  #meetups {
+    margin-top: 50px;
+    margin-bottom: 50px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 1rem;
+  }
+</style>
---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let meetups;
+  import MeetupItem from "./MeetupItem.svelte";
+</script>
+
+{#each meetups as meetup}
+  <MeetupItem
+    title={meetup.title}
+    subtitle={meetup.subtitle}
+    image={meetup.imageUrl}
+    address={meetup.address}
+    description={meetup.description}
+  />
+{/each}
+
+<style></style>
